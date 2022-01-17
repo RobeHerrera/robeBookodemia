c8ade4c9e5fb0577780a9661ac73ae7d620aca53
Roberto Herrera <robeherrera561.2@gmail.com>
2022-01-16 19:56:16 -0600

    Update WelcomeLogin

--- app/src/main/java/com/robe/robebookodemia/WelcomeLogin.kt
+++ app/src/main/java/com/robe/robebookodemia/WelcomeLogin.kt
@@ -1,11 +1,31 @@
 package com.robe.robebookodemia
 
+import Interfaces.NavigationHost
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationHost {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_login)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                //.add(R.id.container.())
+                .commit()
+        }
+    }
+    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
     }
 }
