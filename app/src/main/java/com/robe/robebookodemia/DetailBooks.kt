dc4a06fc72e4516043681ce0d377777106dcf471
Roberto Herrera <robeherrera561.2@gmail.com>
2022-01-18 20:55:47 -0600

    Added 3er layout and menu resource

---
+++ app/src/main/java/com/robe/robebookodemia/DetailBooks.kt
@@ -0,0 +1,11 @@
+package com.robe.robebookodemia
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailBooks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_books)
+    }
+}
---
+++ app/src/main/java/com/robe/robebookodemia/HomeBooks.kt
@@ -0,0 +1,11 @@
+package com.robe.robebookodemia
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeBooks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_books)
+    }
+}
